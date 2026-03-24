80d36a52f6fe90fb079989be647f623adb3bb049
Paul Juhel <p25juhel@imt-atlantique.net>
2026-03-24 10:42:29 +0100

    edit

--- src/abstraction/eq9Distributeur2/Distributeur2AcheteurAO.java
+++ src/abstraction/eq9Distributeur2/Distributeur2AcheteurAO.java
@@ -27,33 +27,35 @@ public class Distributeur2AcheteurAO extends Distributeur2Acteur implements IAch
             //stock actuel
             double stockActuel = this.stock.getOrDefault(choco, 0.0);
             
-            // On se fixe une limite  on veut toujours au moins 50 Tonnes en rayon
-            double seuilDeSecurite = 50.0; 
-
+            // Seuil de sécurité réaliste : 100 tonnes minimum en stock
+            double seuilDeSecurite = 100000.0; // 100 tonnes = 100 000 kg
             
             if (stockActuel < seuilDeSecurite) {
                 
+                // Quantité à acheter : viser 200 tonnes total en stock
+                double quantiteCible = 200000.0; // 200 tonnes
+                double quantiteAcheter = quantiteCible - stockActuel;
                 
-                double quantiteAcheter = 100.0 - stockActuel;
-                
-                
+                // Respecter la quantité minimum pour les appels d'offres
                 if (quantiteAcheter < AppelDOffre.AO_QUANTITE_MIN) {
                     quantiteAcheter = AppelDOffre.AO_QUANTITE_MIN;
                 }
                 
-                this.journal.ajouter("Alerte stock bas pour " + choco.getNom() + ". Lancement d'un AO pour " + quantiteAcheter + "T");
-                
+                this.journal.ajouter("Alerte stock bas pour " + choco.getNom() + 
+                                   " (" + (stockActuel/1000) + "t). Lancement d'AO pour " + 
+                                   (quantiteAcheter/1000) + "t");
                 
                 OffreVente offreRetenue = superviseurAO.acheterParAO(this, this.cryptogramme, choco, quantiteAcheter);
                 
-                
                 if (offreRetenue != null) {
-                    this.journal.ajouter("Victoire : On a acheté à " + offreRetenue.getVendeur().getNom() + " pour " + offreRetenue.getPrixT() + "€/T");
+                    this.journal.ajouter("✅ Achat réussi : " + (quantiteAcheter/1000) + "t de " + 
+                                       choco.getNom() + " à " + offreRetenue.getPrixT() + "€/T chez " + 
+                                       offreRetenue.getVendeur().getNom());
                     
                     this.stock.put(choco, stockActuel + quantiteAcheter);
                     this.indicateurStockTotal.setValeur(this, getStockTotal());
                 } else {
-                    this.journal.ajouter("Échec : Aucune offre intéressante pour " + choco.getNom());
+                    this.journal.ajouter("❌ Échec : Aucune offre acceptable pour " + choco.getNom());
                 }
             }
         }
@@ -64,13 +66,12 @@ public class Distributeur2AcheteurAO extends Distributeur2Acteur implements IAch
         OffreVente meilleureOffre = null;
         double meilleurPrix = Double.MAX_VALUE;
         
+        // Prix maximum acceptable selon la qualité du chocolat demandé
+        double prixMaxAcceptable = 30000.0; // 30 000 €/T maximum
+        
         // On compare les offres concurrentes pour prendre la moins chère 
         for (OffreVente offre : propositions) {
             double prixPropose = offre.getPrixT();
-            
-            
-            // Mais on peut vérifier le prix maximum qu'on est prêt à payer 
-            double prixMaxAcceptable = 2500.0; // À ajuster 
             
             if (prixPropose < meilleurPrix && prixPropose <= prixMaxAcceptable) {
                 meilleurPrix = prixPropose;
@@ -78,7 +79,14 @@ public class Distributeur2AcheteurAO extends Distributeur2Acteur implements IAch
             }
         }
         
-        // On renvoie la meilleure offre au superviseur (ou null si tout le monde est trop cher)
+        if (meilleureOffre != null) {
+            this.journal.ajouter("Offre sélectionnée : " + meilleureOffre.getPrixT() + "€/T pour " + 
+                               (meilleureOffre.getQuantiteT()) + "t de " + 
+                               ((ChocolatDeMarque)meilleureOffre.getProduit()).getNom());
+        } else {
+            this.journal.ajouter("Aucune offre acceptable (prix > " + prixMaxAcceptable + "€/T)");
+        }
+        
         return meilleureOffre;
     }
 }
