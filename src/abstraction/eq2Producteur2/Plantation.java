9ee9fef3c5c8da24e7d680acd51140c679de7bca
Thomas Croizé <thomas.croize@imt-atlantique.net>
2026-04-28 09:54:15 +0200

    Amélioration conditions de vente

--- src/abstraction/eq2Producteur2/Plantation.java
+++ src/abstraction/eq2Producteur2/Plantation.java
@@ -39,7 +39,7 @@ public class Plantation {
                 this.prix_achat = 2000 ;
                 this.prix_vente = 1200 ;
                 this.prix_replantation = 1000 ; // 1 euro par plant
-                this.salaire_employe = 150 ;
+                this.salaire_employe = 30 ;
 
                 break;
  
@@ -50,7 +50,7 @@ public class Plantation {
                 this.prix_achat = 2500;
                 this.prix_vente = 1500 ;
                 this.prix_replantation = 1500 ; // 1.5 euro par plant
-                this.salaire_employe = 150 ;
+                this.salaire_employe = 30 ;
                 break;
 
             case F_HQ:
@@ -60,7 +60,7 @@ public class Plantation {
                 this.prix_achat = 3000 ;
                 this.prix_vente = 1800 ;
                 this.prix_replantation = 1750 ; // 1.75 euro par plant
-                this.salaire_employe = 150 ;
+                this.salaire_employe = 30 ;
                 break;
 
             case F_HQ_E:
@@ -70,7 +70,7 @@ public class Plantation {
                 this.prix_achat = 3000 ;
                 this.prix_vente = 1800 ;
                 this.prix_replantation = 2000 ; // 2 euro par plant
-                this.salaire_employe = 300 ;
+                this.salaire_employe = 60 ;
                 break;
             
             default:
